8f203c7f55fb58c0fd9f44214f2de1e7624efe7a
Lazaro Osee <odiraoseelazaro@students.tukenya.ac.ke>
2024-11-25 15:58:56 +0300

    Updated asto.config.site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oseelabs.org',
+  site: 'https://www.oseelabs.org',
   integrations: [
     svelte(), 
     sitemap({
